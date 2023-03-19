7aca19fe4dbe4c9c5d63ae452cce5703dd3c7d1f
froxyroar <rifkirosada7@gmail.com>
2023-03-19 15:28:13 +0700

    first commit

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // api url
-const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
+const api_url = "https://api.thingspeak.com/channels/2070634/feeds.json?api_key=2BZ2Q21MLOFIGY5C&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -8,7 +8,8 @@ setInterval(() => {
     .then((res) => {
       var field = JSON.stringify(res.feeds[0]);
       var obj = JSON.parse(field);
-      document.getElementById("field-1").innerHTML = obj.field1;
-      document.getElementById("field-2").innerHTML = obj.field2;
+      document.getElementById("distance_cm").innerHTML = obj.field1;
+      document.getElementById("temperature").innerHTML = obj.field2;
+      document.getElementById("kelembapan").innerHTML = obj.field3;
     });
 }, 1000);
